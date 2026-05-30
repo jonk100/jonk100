a6fcf945ee3284e5a52f0716f04b325e9fd47093
Jon K <jonkrausejams@gmail.com>
2026-05-30 14:34:31 -0400

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 import remarkProps from './src/plugins/remark-props.mjs';
 import remarkPreview from './src/plugins/remark-preview.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx({
-    remarkPlugins: [remarkProps, remarkPreview],
-  })],
+  site: 'https://jonk100.netlify.app',
+  integrations: [
+    mdx({
+      remarkPlugins: [remarkProps, remarkPreview],
+    }),
+    sitemap(),
+  ],
 });
